6312777086cdb8a6540cda805a15949327a02748
Avinash Chauhan <139676735+avi94sh@users.noreply.github.com>
2025-10-02 10:09:19 +0000

    arraysummary

---
+++ 01js/arrayrevisom.js
@@ -0,0 +1,23 @@
+const arr2=["hosths","navin","aviansgh"]
+console.log(arr2);
+
+function myfuntion()
+{
+    id=7678;
+    score=9777;
+    console.log(`${id}\n${score}`);
+}
+myfuntion()
+
+//here we make object
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+//dot notation
+console.log(myObj.name); // Output: hitesh
+console.log(myObj.age); // Output: 22
+
+//bracket notation
+console.log(myObj["name"]); // Output: hitesh
+console.log(myObj["age"]); // Output: 22
