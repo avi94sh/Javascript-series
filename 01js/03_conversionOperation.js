85bc6b59a3213f28abdc709004d669dca3fc7218
Avinash Chauhan <139676735+avi94sh@users.noreply.github.com>
2025-10-02 09:08:00 +0000

    03_conversion

---
+++ 01js/03_conversionOperation.js
@@ -0,0 +1,66 @@
+let score=87;
+// number to string
+// console.log(typeof score)
+let valueInString=String(score);
+// console.log(typeof valueInString)
+// console.log(valueInString)
+//string to number
+
+// let name="33";
+// let name="33abc"
+// console.log(typeof name);
+
+// let valueInNymber=Number(name);
+// console.log(valueInNymber,typeof valueInNymber)
+
+// let userLogin=false;
+// let number2=Number(userLogin);
+// let stringN=String(userLogin)
+
+// console.log(stringN,typeof stringN)
+
+// console.log(typeof null);
+// console.log(typeof undefined)
+
+// arithmetic exression
+// console.log(1+2);
+// console.log(1-2);
+// console.log(1*2);
+// console.log(2/1);
+// console.log(2**2);
+// console.log(1%2);
+
+let str1="hello";
+let str2=" aviamsh";
+
+let str3=str1 +str2;
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( ((3 + 4) * 5) % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3;
+// num1=num2=num3=7+7;
+// console.table([num1,num2,num3])
+let num1 = 5;
+let num2 = 3;
+console.log(`sum of ${num1} and ${num2} is ${num1 + num2}`);
+/*
+- Use backticks `...` to enable interpolation.
+- ${num1} will be replaced with the actual value of num1.
+
+
+*/
+//pre increment and psot increment
+let age=566;
+// let age1=++age;
+let age1=age++;
+console.log(age1,age)
