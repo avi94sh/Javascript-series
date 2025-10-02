2e8b6f0f206d99be7137bdbe89c8e71f818bb7a0
Avinash Chauhan <139676735+avi94sh@users.noreply.github.com>
2025-10-02 08:17:58 +0000

    01_varibale

---
+++ 01js/01_variable.js
@@ -0,0 +1,19 @@
+// here are three way to to define the variable
+
+const accountId=123442;
+let accountEmail="hitesh@google.com";
+var accountPassword = "12345"
+// var accountPassword= "hdhhqdhjh" here we can same variable with var keyworfd
+accountCity = "Jaipur"
+accountCity="rahastsdhj"
+let accountState;
+// accountId=15252;
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+// accountPassword="157762"
+accountEmail="avianshchauha@gmail.com"
+// console.log(accountEmail,accountPassword,accountCity)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
