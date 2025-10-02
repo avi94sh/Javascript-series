5201bcec01a2f6659ebaf7d8e8eeb2ff2aa338bd
Avinash Chauhan <139676735+avi94sh@users.noreply.github.com>
2025-10-02 08:35:25 +0000

    01_datatype

---
+++ 01js/01_datatypes.js
@@ -0,0 +1,30 @@
+"use Strict"; //treat all js code as nweer versuion
+//alert('hello')we are using nodejs, not browser
+console.log(3+343);
+
+console.log("aviansh\njdjjjfj");
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+let abeer=null;
+
+/*primitive datattype 
+number=>2**53
+bigint
+string
+boolean
+null=>standalone
+undefined
+symbol=>unique
+
+
+*/
+
+console.log(typeof name);
+console.log(typeof age,typeof isLoggedIn,typeof state,typeof abeer);
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
