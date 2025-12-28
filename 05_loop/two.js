b5f743ed1150655a7b7f5270a95aa50778506349
Avinash Chauhan <139676735+avi94sh@users.noreply.github.com>
2025-12-28 15:07:19 +0000

    two loop

---
+++ 05_loop/two.js
@@ -0,0 +1,23 @@
+//do-while and while\
+let i=1;
+// while (i<10) {
+//     console.log(`value is index is ${i}`);
+//     i = i+2;
+    
+// }
+
+let myArray =["hero", "thor", "superman"]
+
+// let arr=0
+// while(arr < myArray.length)
+// {
+//     console.log(`value is ${myArray[arr]}`)
+//     arr = arr+1
+// }
+
+let score =11;
+do{
+    console.log(`score is ${score}`)
+    score++
+}
+while(score<=10)
